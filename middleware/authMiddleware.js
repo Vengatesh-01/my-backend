1e800544921e16c319a70e26142769c58d4adf83
SelvaRani2509 <selvarani.ug.23.it@francisxavier.ac.in>
2026-02-05 14:28:29 +0530

    Debug: Expose auth error details

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -26,7 +26,7 @@ const protect = async (req, res, next) => {
             next();
         } catch (error) {
             console.log(error);
-            res.status(401).json({ message: 'Not authorized' });
+            res.status(401).json({ message: `Not authorized: ${error.message}` });
         }
     }
 
